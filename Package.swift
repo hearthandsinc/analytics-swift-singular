fbff9b7e894da2917c780c7a6ea8b13430cd022e
Remi Santos <santos.remi@icloud.com>
2024-02-01 12:09:03 +0100

    Setup package and segment plugin

--- Package.swift
+++ Package.swift
@@ -1,23 +1,41 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "analytics-swift-singular",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "analytics-swift-singular",
-            targets: ["analytics-swift-singular"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "analytics-swift-singular"),
-        .testTarget(
-            name: "analytics-swift-singularTests",
-            dependencies: ["analytics-swift-singular"]),
-    ]
+  name: "SegmentSingular",
+  platforms: [
+    .macOS("10.15"),
+    .iOS("13.0"),
+    .tvOS("11.0"),
+    .watchOS("7.1")
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "SegmentSingular",
+      targets: ["SegmentSingular"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(
+      url: "https://github.com/segmentio/analytics-swift.git",
+      from: "1.5.2"
+    ),
+    .package(
+      url: "https://github.com/singular-labs/Singular-iOS-SDK.git",
+      from: "12.0.7"
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "SegmentSingular",
+      dependencies: [
+        .product(name: "Segment", package: "analytics-swift"),
+        .product(name: "Singular", package: "Singular-iOS-SDK")
+      ]),
+  ]
 )
--- Sources/analytics-swift-singular/analytics_swift_singular.swift
+++ Sources/analytics-swift-singular/analytics_swift_singular.swift
@@ -1,2 +1,98 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import Segment
+import Singular
+
+@objc(SEGSingularDestination)
+public class ObjCSegmentSingular: NSObject, ObjCPlugin, ObjCPluginShim {
+  public func instance() -> EventPlugin { return SingularDestination() }
+}
+
+public class SingularDestination: DestinationPlugin, iOSLifecycle {
+  public var key = "Singular"
+
+  public var timeline = Segment.Timeline()
+
+  public var type: Segment.PluginType = .destination
+
+  public var analytics: Segment.Analytics?
+
+
+  // internal config
+  private let isSKANEnabled: Bool
+  private let isManualSKANMode: Bool
+
+  public init(isSKANEnabled: Bool = false, isManualSKANMode: Bool = false) {
+    self.isSKANEnabled = isSKANEnabled
+    self.isManualSKANMode = isManualSKANMode
+    Singular.setWrapperName("Segment", andVersion: "1.2.0")
+  }
+
+  public func update(settings: Settings, type: UpdateType) {
+    // we've already set up this singleton SDK, can't do it again, so skip.
+    guard type == .initial else { return }
+
+    // TODO: Update the proper types
+    guard let singularSettings = settings.integrationSettings(forKey: key) else {
+      analytics?.log(message: "Singular settings could not load")
+      return
+    }
+
+    guard let apiKey = singularSettings["apiKey"] as? String, let secret = singularSettings["secret"] as? String else {
+      analytics?.log(message: "Singular settings does not contain an apiKey or secret")
+      return
+    }
+
+    analytics?.log(message: "Singular settings loaded")
+    
+    if let config = SingularConfig(apiKey: apiKey, andSecret: secret) {
+      config.skAdNetworkEnabled = isSKANEnabled
+      config.manualSkanConversionManagement = isManualSKANMode
+      Singular.start(config)
+
+      analytics?.log(message: "Singular started")
+    }
+  }
+
+  public func track(event: TrackEvent) -> TrackEvent? {
+    if let properties = event.properties?.dictionaryValue as? [String: Any], let revenue = extractRevenue(properties, key: "revenue") {
+      let currency = properties["currency"] as? String
+      analytics?.log(message: "Singular track revenue \(revenue)")
+      Singular.customRevenue(event.event, currency: currency, amount: revenue)
+    } else {
+      analytics?.log(message: "Singular track event \(event.event)")
+      Singular.event(event.event)
+    }
+    return event
+  }
+
+  public func identify(event: IdentifyEvent) -> IdentifyEvent? {
+    if let userId = event.userId, !userId.isEmpty {
+      Singular.setCustomUserId(userId)
+      analytics?.log(message: "Singular identify \(userId)")
+    }
+    return nil
+  }
+
+  public func reset() {
+    analytics?.log(message: "Singular reset")
+    Singular.unsetCustomUserId()
+  }
+}
+
+extension SingularDestination {
+  private func extractRevenue(_ properties: [String: Any], key: String) -> Double? {
+    var revenue: Double? = nil
+    if let revenueProperty = properties[key] {
+      if let revenueProperty = revenueProperty as? String {
+        revenue = Double(revenueProperty)
+      } else if let revenueProperty = revenueProperty as? NSNumber {
+        revenue = revenueProperty.doubleValue
+      }
+    }
+
+    return revenue
+  }
+}
